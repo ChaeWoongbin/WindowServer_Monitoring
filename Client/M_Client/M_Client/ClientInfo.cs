b1df0029cea944dfc0a3087047487caf0b4f0032
N-225\N-225 <jerpi053@gmail.com>
2022-06-20 11:09:09 +0900

    클라_기본정보 test

---
+++ Client/M_Client/M_Client/ClientInfo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace M_Client
+{
+    /// <summary>
+    /// CPU : CPU 사용량
+    /// Memory : 메모리 사용량
+    /// drive : 여유용량
+    /// connect : 연결확인
+    /// </summary>
+    class ClientInfo
+    {
+        // 성능 기본정보
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter memCounter;
+        public DriveInfo storageCheck;
+
+
+        public string CPU = "";
+        public string Memory = "";
+        public string Memory_percent = "";
+        public string drive = "";
+        public bool connect = false;
+
+        public void GetData()
+        {
+            GetCPU();
+            GetMemory();
+            Getdrive();
+            Getconnect();
+        }
+
+
+        private void GetCPU()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            CPU = cpuCounter.NextValue().ToString();
+        }
+        private void GetMemory()
+        {
+            memCounter = new PerformanceCounter("Memory", "Available MBytes");
+            Memory = ((Convert.ToDouble(memCounter.NextValue())) / 1024).ToString("N2");
+            Memory_percent = ram_percentage(RAM_Check()).ToString("N2") + "%";
+        }
+        private void Getdrive()
+        {
+            storageCheck = new DriveInfo(System.IO.Directory.GetCurrentDirectory().ToString());
+            drive = ((double)storageCheck.AvailableFreeSpace / (1024 * 1024 * 1024)).ToString("N2");
+        }
+        private void Getconnect()
+        {
+            connect = false;
+        }
+
+
+
+        private double ram_percentage(double ram_check) // 램 계산
+        {
+            double percent = 0;
+
+            double now = Convert.ToDouble(((RAM_Check() / 1024 / 1024) - ((Convert.ToDouble(memCounter.NextValue())) / 1024)).ToString());
+            double max = Double.Parse((ram_check / 1024 / 1024).ToString("N2"));
+
+            percent = (now / max) * 100;
+
+
+            return percent;
+        }
+
+
+        private double RAM_Check() // 전체램 확인용
+        {
+            double test = 0;
+            try
+            {
+                ManagementClass cls = new ManagementClass("Win32_OperatingSystem");
+                ManagementObjectCollection instances = cls.GetInstances();
+
+                foreach (ManagementObject info in instances)
+                {
+                    test = double.Parse(info["TotalVisibleMemorySize"].ToString());
+                }
+
+                return test;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return 0;
+            }
+        }
+    }
+}
--- Client/M_Client/M_Client/MainWindow.xaml.cs
+++ Client/M_Client/M_Client/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace M_Client
 {
@@ -20,9 +24,54 @@ namespace M_Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 반복
+        private DispatcherTimer Timer;
+        ClientInfo Client = new ClientInfo();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            SetProgram(); // 프로그램 실행
+        }
+
+        private void SetProgram()
+        {
+            try { Client.GetData(); }
+            catch { MessageBox.Show("(couCounter Error) starting restore Regi..."); CMD("lodctr /r"); } // 성능 카운터 레지스트리 초기화
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 0, 10, 0); // 10초당 타이머 실행
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+
+        public void CMD(string command) // 명령프롬프트 실행
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.Verb = "runas";
+            cmd.Start();
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit(1);
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+       
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try { Client.GetData(); }
+            catch { MessageBox.Show("(couCounter Error) starting restore Regi..."); CMD("lodctr /r"); } // 성능 카운터 레지스트리 초기화
+            test.Text = Client.Memory + "%";
         }
     }
 }
